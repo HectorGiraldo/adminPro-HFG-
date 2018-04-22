d7f20ae51a171e97e9ce8a18a595f56ba6f4ef56
Hector <hectorgiraldo0210@gmail.com>
2018-04-21 22:40:51 -0500

    Sección 13 terminada

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
+import { element } from 'protractor';
 
 declare function init_pluggins();
+declare const gapi: any;
+
 
 @Component({
   selector: 'app-login',
@@ -10,14 +16,66 @@ declare function init_pluggins();
 })
 export class LoginComponent implements OnInit {
 
-  constructor( public router: Router) { }
+  email: string;
+  recuerdame: boolean = false;
+
+  auth2: any;
+
+  constructor(
+    public router: Router,
+    public _usuarioService: UsuarioService
+  ) {}
 
   ngOnInit() {
     init_pluggins();
+    this.googleInit();
+
+    this.email = localStorage.getItem('email') || '';
+    if (this.email.length > 1) {
+      this.recuerdame = true;
+    }
   }
 
-  ingresar() {
-    console.log('ingresando');
-    this.router.navigate(['/dashboard']);
+  // tslint:disable-next-line:no-shadowed-variable
+  attachSignin( element ) {
+    this.auth2.attachClickHandler(element, {}, (googleUser) => {
+
+      // let profile = googleUser.getBasicProfile();
+      let token = googleUser.getAuthResponse().id_token;
+
+      this._usuarioService.loginGoogle(token)
+        .subscribe(resp => window.location.href = '#/dashboard');
+
+    });
+
+  }
+
+  googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '29936731848-4pfequf0p73c6qu0743vfgbgrqia5p0b.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+
+      this.attachSignin(document.getElementById('btnGoogle'));
+
+
+    });
+  }
+
+  ingresar(forma: NgForm) {
+
+    if (forma.invalid) {
+      return;
+    }
+
+    let usuario = new Usuario (null, forma.value.email, forma.value.password);
+
+    this._usuarioService.login(usuario, forma.value.recuerdame)
+              .subscribe(ok => this.router.navigate(['/dashboard']) );
+
+
+    // this.router.navigate(['/dashboard']);
   }
 }
--- src/app/login/register.component.ts
+++ src/app/login/register.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
+import { Router } from '@angular/router';
+
+import swal from 'sweetalert2';
+
+
+declare function init_pluggins();
 
 @Component({
   selector: 'app-register',
@@ -6,10 +15,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class RegisterComponent implements OnInit {
+  forma: FormGroup;
 
-  constructor() { }
+  constructor(
+    public _usuarioservice: UsuarioService,
+    public router: Router
+  ) {}
 
-  ngOnInit() {
+  sonIguales(campo1: string, campo2: string) {
+    return(group: FormGroup) => {
+      let pass1 = group.controls[campo1].value;
+      let pass2 = group.controls[campo2].value;
+
+      if (pass1 === pass2) {
+        return null;
+      }
+
+      return {
+        sonIguales: true
+      };
+    };
   }
 
+  ngOnInit() {
+    init_pluggins();
+
+    this.forma = new FormGroup({
+      nombre: new FormControl(null, Validators.required),
+      correo: new FormControl(null, [Validators.required, Validators.email, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
+      password: new FormControl(null, Validators.required),
+      password2: new FormControl(null, Validators.required),
+      condiciones: new FormControl(false)
+    }, {validators: this.sonIguales('password', 'password2')});
+
+    this.forma.setValue({
+      nombre: 'hector',
+      correo: 'test@test.com',
+      password: '123456',
+      password2: '123456',
+      condiciones: true
+    });
+  }
+
+  registrarUsuario() {
+
+    if ( this.forma.invalid ) {
+      return;
+    }
+
+    if ( !this.forma.value.condiciones ) {
+      swal('Importante!', 'Debe aceptar las condiciones!', 'warning');
+      return;
+    }
+
+    let usuario = new Usuario(
+      this.forma.value.nombre,
+      this.forma.value.correo,
+      this.forma.value.password,
+    );
+
+    this._usuarioservice.crearUsuario(usuario)
+        .subscribe(resp => this.router.navigate(['/login']));
+  }
 }
